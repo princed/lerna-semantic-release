b8b5a9822a5f327efc3cbe3085281a26c1488759
Eugene Datsky <eugene@datsky.ru>
2016-12-14 18:58:56 +0300

    fix(lerna): Use @ahfarmer/lerna fork as lerna package
    affects: @princed/lerna-semantic-release, @princed/lerna-semantic-release-io

--- packages/lerna-semantic-release-io/io/lerna.js
+++ packages/lerna-semantic-release-io/io/lerna.js
@@ -1,9 +1,8 @@
-var Repository = require('lerna/lib/Repository');
-var PackageUtilities = require('lerna/lib/PackageUtilities');
+var Repository = require('@ahfarmer/lerna/lib/Repository');
+var PackageUtilities = require('@ahfarmer/lerna/lib/PackageUtilities');
 
 module.exports = {
   getAllPackages: function () {
-    var packagesLocation = new Repository().packagesLocation;
-    return PackageUtilities.getPackages(packagesLocation);
+    return PackageUtilities.getPackages(new Repository());
   }
 };
